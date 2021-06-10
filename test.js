e4e9e4e5d34fed3b45c7f04e3b7979301c2c37b6
wkdewey <wkdewey@gmail.com>
2021-06-10 16:09:28 -0400

    add examples of async tests

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 var assert = require("assert");
+
+beforeEach(function () {
+  return db.clear().then(function () {
+    return db.save([tobi, loki, jane]);
+  });
+});
 describe("Array", function () {
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
@@ -8,5 +14,20 @@ describe("Array", function () {
       setImmediate(done);
       setImmediate(done);
     });
+  });
+});
+describe("User", function () {
+  describe("#save()", function () {
+    it("should save without error", function (done) {
+      var user = new User("Luna");
+      user.save(function (err) {
+        if (err) done(err);
+      });
+    });
+  });
+});
+describe("#find()", function () {
+  it("responds with matching records", function () {
+    return db.find({ type: "User" }).should.eventually.have.length(3);
   });
 });
